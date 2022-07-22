69ab06f1be95f7a378ae0bc9c2efb98e453524ea
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-07-22 15:22:40 -0600

    Non working gate way
    It registers with Eureka

--- CatalogService/Item.cs
+++ CatalogService/Item.cs
@@ -1,8 +1,8 @@
 public class Item
 {
     public long Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
     public decimal Price { get; set; }
     public long Quantity { get; set; }
 
