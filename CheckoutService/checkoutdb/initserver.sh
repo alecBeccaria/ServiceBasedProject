c97bcb1d6426f98a7689b7015ca03f6b198cae04
Poke711 <wmejiapalma@student.neumont.edu>
2022-07-26 17:42:03 -0600

    Fixed Database Creation on sql servers
    Windows to linux conversion issue
    Line endings are different "\r" vs \"r\n"
    renamed some files to avoid spelling mistakes

--- CheckoutService/checkoutdb/initserver.sh
+++ CheckoutService/checkoutdb/initserver.sh
@@ -1,6 +1,7 @@
 # Wait to be sure that SQL Server came up
-sleep 90s
-
+echo "starting sql server"
+sleep 30s
+echo "running sql script"
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P abc123!!@ -d master -i create-database.sql
