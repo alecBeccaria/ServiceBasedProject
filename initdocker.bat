e6905e881ed80e6c0b70221b58dc69f43dcddbba
Poke711 <wmejiapalma@student.neumont.edu>
2022-07-27 15:14:34 -0600

    [READ THE README] swarm created 3 userservices

---
+++ initdocker.bat
@@ -0,0 +1,17 @@
+@echo DOTNET builds
+dotnet build -c Release
+dotnet publish -c Release
+
+@echo building docker images
+docker-compose build --no-cache 
+@echo "Docker build complete"
+
+@echo "creating docker swarm network"
+docker network create --driver overlay netSEN3
+@echo "Creating docker swarm"
+docker swarm init
+@echo "Docker swarm created"
+@echo "Deploying docker swarm!"
+docker stack deploy -c docker-compose.yml packagedAPIs
+
+@echo "finished deploying docker swarm"
